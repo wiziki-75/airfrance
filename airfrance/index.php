1df5dcfa8fd78191309c45ad75ae0d67ed6b90d6
Anthony Guerrand <59973442+wiziki-75@users.noreply.github.com>
2023-10-26 15:33:52 +0200

    Update index.php

--- airfrance/index.php
+++ airfrance/index.php
@@ -6,22 +6,20 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Site Scoalrité</title>
+	<title>Air France</title>
 	<meta charset="utf-8">
 </head>
 <body>
 <center>
-	<h1> Site Scolarité IRIS </h1>
+	<h1> Air France Administration </h1>
 	<a href="index.php?page=1"> 
 			<img src="images/logo.png" height="100" width="100"> </a>
 	<a href="index.php?page=2"> 
-			<img src="images/classes.png" height="100" width="100"> </a>
+			<img src="images/avion.png" height="100" width="100"> </a>
 	<a href="index.php?page=3"> 
-			<img src="images/professeur.png" height="100" width="100"> </a>
+			<img src="images/pilote.png" height="100" width="100"> </a>
 	<a href="index.php?page=4"> 
-			<img src="images/etudiant.png" height="100" width="100"> </a>
-	<a href="index.php?page=5"> 
-			<img src="images/enseignement.png" height="100" width="100"> </a>
+			<img src="images/vol.png" height="100" width="100"> </a>
 
 	<?php 
 	if (isset($_GET['page'])){
@@ -31,10 +29,6 @@
 	}
 	switch ($page){
 		case 1 : require_once ("home.php"); break; 
-		case 2 : require_once ("gestion_classes.php"); break; 
-		case 3 : require_once ("gestion_professeurs.php"); break; 
-		case 4 : require_once ("gestion_etudiants.php"); break; 
-		case 5 : require_once ("gestion_enseignements.php"); break; 
 		default : require_once("erreur.php"); break;
 	}
 	?>
