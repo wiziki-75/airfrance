1534e1c9ac6733df790d72b88409ab9444da4e10
Anthony Guerrand <59973442+wiziki-75@users.noreply.github.com>
2023-10-26 15:29:09 +0200

    Add files via upload

---
+++ airfrance/home.php
@@ -0,0 +1,9 @@
+<h2> Ecole IRIS </h2>
+
+<br>
+<img src="images/iris.png" height="300" width="500">
+<br>
+<a href="https:/ecoleiris.fr"> Le site de l'école </a>
+<br>
+<br>
+<br>
---
+++ airfrance/modele/modele.class.php
@@ -0,0 +1,25 @@
+<?php
+	class Modele {
+		private $unPDO ; //php data object 
+
+		public function __construct (){
+			try{
+			$url ="mysql:host=localhost:8889;dbname=airfrance"; 
+			$user = "root"; 
+			$mdp =""; 
+			//instanciation de la classe PDO 
+			$this->unPDO = new PDO($url, $user, $mdp);
+			}
+			catch (PDOException $exp){
+				echo "Erreur connexion BDD : ".$exp->getMessage (); 
+			}
+		}
+	}
+?>
+
+
+
+
+
+
+
