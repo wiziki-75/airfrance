6fd46f818c4bd0efd1f5248cf6c1cb7e2ea226e4
Anthony Guerrand <59973442+wiziki-75@users.noreply.github.com>
2023-10-26 17:50:46 +0200

    Delete airfrance directory

--- airfrance/erreur.php
+++
@@ -1,8 +0,0 @@
-<h2> Page introuvable </h2>
-<br>
-<img src="images/erreur.png" height="300" width="500">
-<br>
-<a href="index.php?page=1"> Revenir à l'accueil </a>
-<br>
-<br>
-<br>
--- airfrance/home.php
+++
@@ -1,7 +0,0 @@
-<br>
-<img src="images/airfrance.png" height="300" width="500">
-<br>
-<a href="https://wwws.airfrance.fr/"> Le site d'Air France </a>
-<br>
-<br>
-<br>
